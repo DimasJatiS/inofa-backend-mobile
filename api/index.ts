58a454aebdf6911a65e5dc9a9f477c780eed6499
Dimas Jati Satria <jati.satria000@gmail.com>
2025-12-18 06:20:44 +0700

    resolve crashed functions

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-// api/index.js - Entry point untuk Vercel serverless (CommonJS)
-const app = require("../dist/server.js");
-
-// Handle export default vs module.exports
-module.exports = app.default ?? app;

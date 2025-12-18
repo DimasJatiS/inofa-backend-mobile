4f664505194d7b9dac2b6590fec5f32bdeacaa90
Dimas Jati Satria <jati.satria000@gmail.com>
2025-12-18 10:23:46 +0700

    chore: ignore dist output

--- dist/routes/whatsapp.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const whatsapp_controller_1 = require("../controllers/whatsapp.controller");
-const router = (0, express_1.Router)();
-router.get('/link', whatsapp_controller_1.getWhatsappLink);
-exports.default = router;
